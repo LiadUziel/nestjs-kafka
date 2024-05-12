6d996edbb01a9147f3fbfb824b296bb146d68489
Liad Uziel <liadu@rafael.co.il>
2024-05-12 18:29:23 +0300

    create consumer service

--- src/kafka/kafka.module.ts
+++ src/kafka/kafka.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ProducerService } from './producer/producer.service';
+import { ConsumerService } from './consumer/consumer.service';
 
 @Module({
-  providers: [ProducerService],
-  exports: [ProducerService],
+  providers: [ProducerService, ConsumerService],
+  exports: [ProducerService, ConsumerService],
 })
 export class KafkaModule {}
